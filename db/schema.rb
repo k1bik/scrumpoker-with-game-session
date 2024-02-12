7176e0764f1882705f1cd9d0c28a08048db37076
Kirill Biller <kirill.biller@jetruby.com>
2024-02-12 21:52:32 +0300

    added navbar and some models

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_04_170804) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_12_174937) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string "name"
+    t.string "esitmates"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
