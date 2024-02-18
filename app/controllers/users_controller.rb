4c8e28d17bd9849c957ac8fdbee05f5f18e9d196
Kirill Biller <kirill.biller@jetruby.com>
2024-02-18 18:53:00 +0300

    created user show layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def show
+    current_user = User.first
+  end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,7 @@ module ScrumpokerWithGameSession
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.i18n.default_locale = :en
   end
 end
